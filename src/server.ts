48432d1ce9fb29460d20ecc72bda4d757df131e0
Guilhermecheng <guilherme.cheng@gmail.com>
2024-02-18 02:50:06 -0300

    creating transactions

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*')
-  return transactions
-})
-
-app.get('/hello2', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-  return transactions
-})
-
-app.get('/hello3', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
